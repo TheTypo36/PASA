768129a82149f62d32eef19585ea77803679f680
Ashish Raghuvanshi <thetypo36@gmail.com>
2024-03-02 18:05:26 +0530

    added routes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,7 +5,7 @@ import { Nav } from "./Nav";
 import { CarouselComponent } from "./CarouselCompnent";
 import { Products } from "./Products";
 import { Cart } from "./Cart";
-import { BrowserRouter, Routes, Route, useActionData } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [productCount, setProductCount] = useState(0);
